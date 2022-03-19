3f44695e66d46a014f71be1074fb2caa28769b77
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2022-03-19 01:42:39 +0100

    refactored forbidden server

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"github.com/gorilla/mux"
-	"html/template"
-	"log"
-	"net/http"
-	"time"
-)
-
-var assets map[string]*template.Template
-
-func renderTemplate() {
-	if assets == nil {
-		assets = make(map[string]*template.Template)
-	}
-	assets["index"] = template.Must(template.ParseFiles("assets/index.html", "assets/styles.css"))
-}
+import "net/http"
 
 func main() {
-	router := mux.NewRouter().StrictSlash(false)
-	fileServer := http.FileServer(http.Dir("assets"))
-	router.Handle("/", fileServer)
-	srv := &http.Server{
-		Handler:      router,
-		Addr:         "127.0.0.1:8080",
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-	log.Fatal(srv.ListenAndServe())
+	http.ListenAndServe(":3000", http.FileServer(http.Dir("assets")))
 }
